6829588604ddf89a1f52c02bbbc6bee78d30078b
Sohag Rana <sohagrana921@gmail.com>
2024-01-22 20:28:00 +0600

    Navbar done

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -1,15 +1,16 @@
 const TopNav = () => {
   return (
-    <div className="flex items-center justify-between px-1 md:px-10 py-4 border-b-2">
+    <div className="flex items-center justify-between px-4 md:px-10 py-4 border-b-2">
       <div className="flex items-center md:items-end gap-5">
         <img
           src="https://static.thenounproject.com/png/128251-200.png"
           alt="logo"
           className="w-[25px] h-[25px] md:w-[50px] md:h-[50px]"
         />
-        <h1 className="hidden md:block md:text-3xl font-serif font-bold ">
-          House Hunter
-        </h1>
+        <div className="flex flex-col items-center ">
+          <h1 className=" md:text-3xl font-serif font-bold ">House Hunter</h1>
+          <h6 className="text-xs hidden md:block">House rent solution</h6>
+        </div>
       </div>
       <div className="flex gap-2 md:gap-8">
         <button className="btn btn-xs md:btn-lg btn-outline md:text-xl">
--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,53 +1,79 @@
 import TopNav from "../../components/TopNav/TopNav";
-
+import {
+  HiHome,
+  HiOutlineViewList,
+  HiClipboardList,
+  HiOutlineTable,
+} from "react-icons/hi";
 const Navbar = () => {
-  const navlinks = (
+  const navLinks = (
     <>
-      <li>Home</li>
-      <li>Home</li>
-      <li>Home</li>
-      <li>Home</li>
+      <li>
+        <span>
+          <HiHome></HiHome> Home
+        </span>
+      </li>
+      <li>
+        <span>
+          <HiOutlineViewList />
+          Property List
+        </span>
+      </li>
+      <li>
+        <span>
+          <HiClipboardList />
+          Article
+        </span>
+      </li>
+      <li>
+        <span>
+          <HiOutlineTable />
+          About Us
+        </span>
+      </li>
     </>
   );
   return (
     <div>
       <TopNav />
-      <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+      <div className="bg-base-100 border-b-2 flex md:flex-col py-2 md:py-4 justify-between">
+        {/* DropDown Menu Here */}
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              {navlinks}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navLinks}
+          </ul>
+        </div>
+
+        <div className="flex justify-around items-center px-5">
+          <div className=" hidden lg:flex">
+            <ul className="menu menu-horizontal text-2xl gap-4 px-1">
+              {navLinks}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">{navlinks}</ul>
-        </div>
-        <div className="navbar-end">
+
           <div className="form-control">
             <input
               type="text"
-              placeholder="Search"
+              placeholder="Search here"
               className="input input-bordered w-24 md:w-auto"
             />
           </div>
